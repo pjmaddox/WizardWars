3425d73876558cc8e7b209ef6846f89c10f57745
William Epstein <epstein.w@husky.neu.edu>
2020-01-08 20:44:29 -0500

    Added input manager for abilities

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    //Todo: USE CROSS PLATFORM INPUT MANAGER!!!!!!!!!!!!!!!!!!!!!
+    public string[] acceptedInputs;
+    public AbilityBase[] abilities;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //assumes correct orderring for strings -> abilities
+        for (int i = 0; i < acceptedInputs.Length; i++)
+        {
+            if(Input.GetButtonDown(acceptedInputs[i]))
+            {
+                abilities[i].Activate();
+            }
+        }
+    }
+}
